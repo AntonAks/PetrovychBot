e6a7e2c10c1b1bd9d7de02492b0d15ed0b515d72
AntonAks <antonaks84@gmail.com>
2021-04-28 07:25:24 +0300

    change async tacks scheduler

--- run.py
+++ run.py
@@ -1,45 +1,37 @@
-import time
+import asyncio
+import aioschedule
 import settings
 from aiogram import executor
-from multiprocessing.context import Process
 from logic import bot, dp
 from data_collector import NewsCollector, store_currency_rates
 
 
+async def task_store_currency_rates():
+    store_currency_rates()
+
+
+async def task_collect_news():
+    NewsCollector.collect_news_data()
+
+
+async def scheduler():
+    aioschedule.every(settings.CURRENCY_RELOAD_TIME).seconds.do(task_store_currency_rates)
+    aioschedule.every(settings.NEWS_RELOAD_TIME).seconds.do(task_collect_news)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
 async def on_shutdown(dp):
-    await bot.send_message(settings.ACCESS_ID, "Я умер!")
-    await bot.close()
+    await bot.send_message(settings.ACCESS_ID, "I'm dead... :(")
 
 
 async def on_startup(dp):
     await bot.send_message(settings.ACCESS_ID, "Привет, я Петрович!")
-
-
-def start_process():
-    p1 = Process(target=CurrencyRatesCollector.start_parsing, args=())
-    p2 = Process(target=NewsCollectorProcess.start_parsing, args=())
-    p1.start()
-    p2.start()
-
-
-class CurrencyRatesCollector:
-    @staticmethod
-    def start_parsing():
-        while True:
-            store_currency_rates()
-            time.sleep(settings.CURRENCY_RELOAD_TIME)
-
-
-class NewsCollectorProcess:
-    @staticmethod
-    def start_parsing():
-        while True:
-            NewsCollector.collect_news_data()
-            time.sleep(settings.NEWS_RELOAD_TIME)
+    asyncio.create_task(scheduler())
 
 
 if __name__ == '__main__':
-    start_process()
     try:
         executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
     except Exception as e:

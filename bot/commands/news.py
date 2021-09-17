9a12621f492face0d9a99d6141727850f3e4e430
AntonAks <antonaks84@gmail.com>
2021-09-17 16:57:53 +0300

    add try for get_news

--- bot/commands/news.py
+++ bot/commands/news.py
@@ -4,12 +4,20 @@ from db import news_collection
 def get_news():
 
     mongo_cursor = news_collection.find().limit(1).sort([('$natural', -1)])
-    last_news = list(mongo_cursor)[0]
 
-    result = last_news['liga_news']['post_urls'][:5] + \
-             last_news['kor_news_world']['post_urls'][:5] + \
-             last_news['ain_news_it']['post_urls'][:5] + \
-             last_news['euro_news_world']['post_urls'][:5] + \
-             last_news['dou_news_it']['post_urls'][:5]
+    try:
+        last_news = list(mongo_cursor)[0]
+        result = last_news['liga_news']['post_urls'][:5] + \
+                 last_news['kor_news_world']['post_urls'][:5] + \
+                 last_news['ain_news_it']['post_urls'][:5] + \
+                 last_news['euro_news_world']['post_urls'][:5] + \
+                 last_news['dou_news_it']['post_urls'][:5]
+    except TypeError:
+        last_news = list(mongo_cursor)[1]
+        result = last_news['liga_news']['post_urls'][:5] + \
+                 last_news['kor_news_world']['post_urls'][:5] + \
+                 last_news['ain_news_it']['post_urls'][:5] + \
+                 last_news['euro_news_world']['post_urls'][:5] + \
+                 last_news['dou_news_it']['post_urls'][:5]
 
     return result

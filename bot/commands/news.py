c2c878c94af0529caae0b63fdcd56acf75fd845e
AntonAks <antonaks84@gmail.com>
2021-09-17 16:54:47 +0300

    clean one news source

--- bot/commands/news.py
+++ bot/commands/news.py
@@ -6,11 +6,10 @@ def get_news():
     mongo_cursor = news_collection.find().limit(1).sort([('$natural', -1)])
     last_news = list(mongo_cursor)[0]
 
-    result = last_news['liga_news']['post_urls'][:4] + \
-             last_news['kor_news_world']['post_urls'][:4] + \
-             last_news['ain_news_it']['post_urls'][:4] + \
-             last_news['liga_news_fin']['post_urls'][:4] + \
-             last_news['euro_news_world']['post_urls'][:4] + \
-             last_news['dou_news_it']['post_urls'][:4]
+    result = last_news['liga_news']['post_urls'][:5] + \
+             last_news['kor_news_world']['post_urls'][:5] + \
+             last_news['ain_news_it']['post_urls'][:5] + \
+             last_news['euro_news_world']['post_urls'][:5] + \
+             last_news['dou_news_it']['post_urls'][:5]
 
     return result
